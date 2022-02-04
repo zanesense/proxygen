d0de6bf36f4c4f8df26e65c1f51d01d2c6fe3949
zanez <saimalixd@gmail.com>
2022-02-04 13:40:21 -0800

    Add files via upload

---
+++ proxygen.py
@@ -0,0 +1,49 @@
+import os,sys
+
+try:
+    import requests
+    from huepy import *
+except:
+    print("required modules missing, installing..")
+    os.system("pip install huepy requests")
+
+print(green(r'''                                                      
+_____________  _______  ______.__. ____   ____   ____  
+\____ \_  __ \/  _ \  \/  <   |  |/ ___\_/ __ \ /    \ 
+|  |_> >  | \(  <_> >    < \___  / /_/  >  ___/|   |  \
+|   __/|__|   \____/__/\_ \/ ____\___  / \___  >___|  /
+|__|                     \/\/   /_____/      \/     \/ 
+
+'''))
+
+types = input(info("Enter proxy type (socks/http)\n>> "))
+if types == "socks":
+    proxy_type = "socks4,socks5"
+elif types == "http":
+    proxy_type = "http"
+elif len(types) == 0:
+    print(bad("No input was provided, exiting.."))
+    sys.exit()
+else:
+    print(bad("Bad proxy type, exiting.."))
+    sys.exit()
+
+proxy_country = input(info("Enter proxy countries (e.g ru,us,cn,nl)\n>> "))
+if len(proxy_country) == 0:
+    print(bad("No input was provided, exiting.."))
+    sys.exit()
+
+proxy_limit = input(info("How many proxies to generate? (min. 10)\n>> "))
+if len(proxy_limit) == 0:
+    print(bad("No input was provied, exiting.."))
+    sys.exit()
+
+def gen():
+    r=requests.get("https://www.proxyscan.io/api/proxy?last_check=9800&country="+proxy_country+"&uptime=80&ping=200&limit="+proxy_limit+"&type="+proxy_type)
+    with open("output.json", "w") as f:
+        f.write(r.text)
+    print(good("Wrote the output to output.json in the current dir."))
+
+gen()
+
+
